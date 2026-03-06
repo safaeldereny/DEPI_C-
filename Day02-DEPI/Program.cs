e098d7090810e7ccba4df471fdafc7c8f79fe94f
Safa Mahmoud Eldreeny <smhmwdbdaljwad@gmail.com>
2026-03-06 15:12:36 +0200

    Add files via upload

---
+++ Day02-DEPI/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Person
+{
+    public string Name;
+}
+namespace CsharpDay02Giza02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+   #region Problem 1
+    // // define an intger x = 10
+    // int x = 10;
+    // // define an integer y = 20;
+    // int y = 20;
+    // /*
+    // Calculate the sum of x and y 
+    // and store it in variable sum
+    // */
+                
+    // int sum = x + y ;
+    // // Print the sum to the console
+    // Console.WriteLine(sum);
+    #endregion
+   #region Problem 2
+    // int x = 10;
+    // Console.WriteLine(x+y);
+    // y is not declare 
+    #endregion  
+
+   #region Problem 3      
+    // Person p1 = new Person();
+    // p1.Name = "Safa";
+
+    // Person p2 = p1;
+
+    // Console.WriteLine("Person1.Name = " + p1.Name);
+    // Console.WriteLine("Person2.Name = "+p2.Name);
+
+    // p2.Name = "Salma";
+
+    // Console.WriteLine("Person1.Name = "+p1.Name);
+    // Console.WriteLine("Person2.Name = "+p2.Name);
+    #endregion   
+
+   #region Problem 4
+    // int a = 15;
+    // int b = 4;
+
+    // int sum = a + b ;
+    // int difference = a - b ;
+    // int product = a * b ;
+    // double division = (double) a / b;
+    // int reminder = a % b; 
+
+    // Console.WriteLine("Sum = " +sum);
+    // Console.WriteLine("Difference = " +difference);
+    // Console.WriteLine("Product = " +product);
+    // Console.WriteLine("Division = " +division);
+    // Console.WriteLine("Reminder = " +reminder);
+
+    #endregion
+
+   #region Problem 5
+    // Console.Write("Enter a number:");
+    // int num = int.Parse(Console.ReadLine());
+
+    // if (num > 10 && num % 2 == 0)
+    //     {
+    //         Console.WriteLine("The number is greater than 10 and even.");
+    //     }
+    // else
+    //     {
+    //         Console.WriteLine("The number does not meet both conditions.");
+                
+    //     }    
+   #endregion   
+
+   #region 
+    // Console.Write("Enter a decimal number(double)");
+    // double input = double.Parse(Console.ReadLine());
+
+    // int ExplicitCast = (int)input;
+
+    // double ImplicitCast = ExplicitCast;
+
+    // Console.WriteLine("Original double value: " +input);
+    // Console.WriteLine("After explicit cast to int :"+ExplicitCast);
+    // Console.WriteLine("After implicit cast back to double: "+ImplicitCast);
+   #endregion
+   #region Problem 6
+    // Console.WriteLine("Enter your age:");
+    // int age = int.Parse(Console.ReadLine());
+
+    // if (age > 0)
+    //     {
+    //         Console.WriteLine("Your age is valid: "+age);       
+    //     }
+    // else
+    //     {
+    //         Console.WriteLine("Invalid input!! Please enter a positive number."); 
+    //     }
+   #endregion
+   #region Problem 7
+   
+   #endregion
+        }
+    }
+    
+}
